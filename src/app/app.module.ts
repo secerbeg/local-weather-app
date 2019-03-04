14f2ad0ffed48bd3862b454c5b486aa6a3635dad
mirza.secerbegovic <secerbeg@gmail.com>
2019-03-04 13:47:40 -0800

    Updates to local app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import {WeatherService} from "./weather/weather.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
