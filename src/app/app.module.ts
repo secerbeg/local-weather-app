5f7fed52be11926707529a5e0316b21adbf5b70a
mirzasecerbegovic <secerbeg@gmail.com>
2019-03-15 00:06:26 -0700

    Merge branch 'master' of https://github.com/secerbeg/local-weather-app
    # Conflicts:
    #	package-lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-import {WeatherService} from "./weather/weather.service";
-import {HttpClientModule} from "@angular/common/http";
+import {WeatherService} from './weather/weather.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CurrentWeather} from '../interfaces/current-weather';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+
   constructor(private httpClient: HttpClient) { }
+
+  getCurrentWeather(city: string, country: string){
+    return this.httpClient.get<CurrentWeatherData>();
+  }
 
 }
